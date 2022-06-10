4a7815fd002bcf8d7aed709120116b957d3c70bb
roccaccu <naimadzhun@gmail.com>
2022-06-10 17:35:07 +0800

    last update

--- src/libs/js/swiper.js
+++ src/libs/js/swiper.js
@@ -15,3 +15,30 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+
+// Second slider
+
+var swiper = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// const burger = document.querySelector('.burger-wrapper')
+
+const burger = document.querySelector('.burger-wrapper')
+const burgerBorder = document.querySelector('.header-nav-center__burger')
+
+burgerBorder.addEventListener('click', function () {
+  // burger.style.display = 'none'
+  burger.classList.toggle('active-burger')
+})
